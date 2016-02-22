72c4c9da6e6203e272e349ae5e337d23618b40c1
Pat David <patdavid@gmail.com>
2016-02-22 12:31:56 -0600

    Updated sg-luminosity-masks to work with GIMP 2.9

--- sg-luminosity-masks.scm
+++ sg-luminosity-masks.scm
@@ -5,6 +5,10 @@
 ; Adapted from tutorial by Tony Kuyper (originally for PS)
 ; http://goodlight.us/writing/luminositymasks/luminositymasks-1.html 
 
+; 2016-02-22 - Pat David
+; Adapted for GIMP 2.9.x by updating some deprecated pdb calls to
+; their new versions.
+
 ; This program is free software; you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
 ; the Free Software Foundation; either version 2 of the License, or
@@ -28,9 +32,9 @@
         (MM #f)
         (MMM #f)
         (masks '())
-        (name (car (gimp-drawable-get-name drawable))) )
+        (name (car (gimp-item-get-name drawable))) )
     (set! masks (cons L masks))
-    (gimp-drawable-set-name L (string-append "L-" name))
+    (gimp-item-set-name L (string-append "L-" name))
     (gimp-selection-none image)
     ; paste uses luminosity desaturation method
     (let ((buffer (car (gimp-edit-named-copy drawable "temp"))))
@@ -38,43 +42,43 @@
       (gimp-buffer-delete buffer) )
     (set! D (car (gimp-channel-copy L)))
     (gimp-image-insert-channel image D 0 1)
-    (gimp-drawable-set-name D (string-append "D-" name))
+    (gimp-item-set-name D (string-append "D-" name))
     (gimp-invert D)
     (set! masks (cons D masks))
 
     (set! DD (car (gimp-channel-copy D)))
     (gimp-image-insert-channel image DD 0 2)
-    (gimp-drawable-set-name DD (string-append "DD-" name))
+    (gimp-item-set-name DD (string-append "DD-" name))
     (gimp-channel-combine-masks DD L CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons DD masks))
 
     (set! DDD (car (gimp-channel-copy DD)))
     (gimp-image-insert-channel image DDD 0 3)
-    (gimp-drawable-set-name DDD (string-append "DDD-" name))
+    (gimp-item-set-name DDD (string-append "DDD-" name))
     (gimp-channel-combine-masks DDD L CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons DDD masks))
 
     (set! LL (car (gimp-channel-copy L)))
     (gimp-image-insert-channel image LL 0 1)
-    (gimp-drawable-set-name LL (string-append "LL-" name))
+    (gimp-item-set-name LL (string-append "LL-" name))
     (gimp-channel-combine-masks LL D CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons LL masks))
     
     (set! LLL (car (gimp-channel-copy LL)))
     (gimp-image-insert-channel image LLL 0 2)
-    (gimp-drawable-set-name LLL (string-append "LLL-" name))
+    (gimp-item-set-name LLL (string-append "LLL-" name))
     (gimp-channel-combine-masks LLL D CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons LLL masks))
     
     (set! M (car (gimp-channel-copy L)))
     (gimp-image-insert-channel image M 0 3)
-    (gimp-drawable-set-name M (string-append "M-" name))
+    (gimp-item-set-name M (string-append "M-" name))
     (gimp-channel-combine-masks M D CHANNEL-OP-INTERSECT 0 0)
     (set! masks (cons M masks))
 
     (set! MM (car (gimp-channel-copy LL)))
     (gimp-image-insert-channel image MM 0 3)
-    (gimp-drawable-set-name MM (string-append "MM-" name))
+    (gimp-item-set-name MM (string-append "MM-" name))
     (gimp-invert MM)
     (gimp-channel-combine-masks MM DD CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons MM masks))
@@ -82,26 +86,35 @@
     (set! MMM (car (gimp-channel-copy LLL)))
     (gimp-image-insert-channel image MMM 0 3)
     (gimp-invert MMM)
-    (gimp-drawable-set-name MMM (string-append "MMM-" name))
+    (gimp-item-set-name MMM (string-append "MMM-" name))
     (gimp-channel-combine-masks MMM DDD CHANNEL-OP-SUBTRACT 0 0)
     (set! masks (cons MMM masks))
     
-    (gimp-selection-load orig-sel)
+    ;(gimp-selection-load orig-sel)
+    (gimp-image-select-item image CHANNEL-OP-REPLACE orig-sel)
     (if (or (= (car (gimp-selection-is-empty image)) TRUE)
             (= (car (gimp-drawable-mask-intersect drawable)) FALSE) )
       (gimp-selection-all image) )
-    (gimp-rect-select image
-                      (car (gimp-drawable-offsets drawable))
-                      (cadr (gimp-drawable-offsets drawable))
-                      (car (gimp-drawable-width drawable))
-                      (car (gimp-drawable-height drawable))
-                      CHANNEL-OP-INTERSECT 0 0 )
+    ;(gimp-rect-select image
+    ;                  (car (gimp-drawable-offsets drawable))
+    ;                  (cadr (gimp-drawable-offsets drawable))
+    ;                  (car (gimp-drawable-width drawable))
+    ;                  (car (gimp-drawable-height drawable))
+    ;                  CHANNEL-OP-INTERSECT 0 0 )
+    (gimp-image-select-rectangle image 
+                                 CHANNEL-OP-INTERSECT
+                                 (car (gimp-drawable-offsets drawable))
+                                 (cadr (gimp-drawable-offsets drawable))
+                                 (car (gimp-drawable-width drawable))
+                                 (car (gimp-drawable-height drawable)) )
+
     (gimp-selection-invert image)
     (unless (= (car (gimp-selection-is-empty image)) TRUE)
       (map (lambda (x) (gimp-edit-fill x WHITE-FILL)
                        (gimp-invert x) )
            masks ))
-    (gimp-selection-load orig-sel)
+    ;(gimp-selection-load orig-sel)
+    (gimp-image-select-item image CHANNEL-OP-REPLACE orig-sel)
     (gimp-image-remove-channel image orig-sel)
     )
   (gimp-image-set-active-layer image drawable)
